c8dba5b1026517a68f82fe5cecd663eb48f1a6c5
nicholasfalter00 <nicholasfalter00@gmail.com>
2024-04-16 22:18:47 -0700

    Bootstrap setup

---
+++ NicFWebApp/Pages/Testing.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NicFWebApp.Pages
+{
+    public class TestingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
