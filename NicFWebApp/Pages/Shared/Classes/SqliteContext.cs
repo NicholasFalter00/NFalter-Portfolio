937de74584f137a0ece2cb69aa9abd6e685c7809
nicholasfalter00 <nicholasfalter00@gmail.com>
2024-06-24 22:44:10 -0700

    Removed unused code

--- NicFWebApp/Pages/Shared/Classes/SqliteContext.cs
+++ NicFWebApp/Pages/Shared/Classes/SqliteContext.cs
@@ -10,19 +10,5 @@ namespace NicFWebApp.Pages.Shared.Classes
 
         public DbSet<Experience> Experiences { get; set; }
         public DbSet<Task> Tasks { get; set; }
-
-        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseSqlite(@"Data Source=C:\Users\nicho\source\repos\NicFWebApp\NicFWebApp\db");*/
-
-        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            var dbPath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "Database.db");
-            optionsBuilder.UseSqlite("Filename = " + dbPath);
-            base.OnConfiguring(optionsBuilder);
-        }*/
-
-        /*{
-            base.OnConfiguring(optionsBuilder);
-        }*/
     }
 }
